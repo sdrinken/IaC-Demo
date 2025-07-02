530a058e3accd13de93499f31157c0908456fe0f
MSX <sdrinken@users.noreply.github.com>
2025-07-02 06:57:37 +0200

    Update dns-private-link.tf

--- AKS-DNS/dns-private-link.tf
+++ AKS-DNS/dns-private-link.tf
@@ -1,14 +1,18 @@
-# Private DNS Zone for AKS Private Cluster API
-resource "azurerm_private_dns_zone" "aks_private_dns" {
-  name                = "privatelink.westeurope.azmk8s.io"
-  resource_group_name = "aks-private-rg"
+provider "azurerm" {
+  features {}
 }
 
-# Virtual Network Link to AKS VNet
-resource "azurerm_private_dns_zone_virtual_network_link" "aks_dns_link" {
+# Private DNS Zone for AKS private API
+resource "azurerm_private_dns_zone" "aks_private_dns" {
+  name                = "privatelink.westeurope.azmk8s.io"
+  resource_group_name = var.resource_group_name
+}
+
+# VNet Link to the AKS VNet
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   name                  = "aks-private-dns-link"
-  resource_group_name   = "aks-private-rg"
+  resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.aks_private_dns.name
-  virtual_network_id    = "aks-vnet"
+  virtual_network_id    = var.aks_vnet_id
   registration_enabled  = false
 }
